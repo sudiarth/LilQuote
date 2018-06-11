2f388405ea795a55a58f18cce310bb55e772effd
Kadek Sudiartha <sudieartha@gmail.com>
2018-06-11 11:16:44 +0800

    styling

--- db/data_layer.py
+++ db/data_layer.py
@@ -1,48 +1,60 @@
-from sqlalchemy import or_, and_
+from sqlalchemy import or_, and_, desc
 from db.base import DbManager
-from db.models import User
+from db.models import User, Quote
 
 def get_all_quotes():
     db = DbManager()
-    return db.open().query(Quote).all()
+    return db.open().query(Quote).order_by(desc(Quote.created_at)).all()
 
 def get_all_quotes_for(user_id):
     db = DbManager()
-    return db.open().query(Quote).filter(Quote.user_id == user_id).all()
+    return db.open().query(Quote).order_by(desc(Quote.created_at)).filter(Quote.user_id == user_id).all()
 
 def get_quote(quote_id):
-    db = DbManager()
-    return db.open().query(Quote).filter(Quote.id == quote_id).one()
+    try:
+        db = DbManager()
+        return db.open().query(Quote).filter(Quote.id == quote_id).one()
+    except:
+        pass
 
 def search_by_user_or_email(query):
-    db = DbManager()
-    return db.open().query(User).filter(or_(User.username.like('%{}%'.format(query)), User.email.like('%{}%'.format(query)))).all()
+    try:
+        db = DbManager()
+        return db.open().query(User).filter(or_(User.name.like('%{}%'.format(query)), User.email.like('%{}%'.format(query)))).all()
+    except:
+        pass
 
 def create_quote(user_id, content):
-    db = DbManager()
-    quote = Quote()
-    quote.content = content
-    return db.save(quote)
+    try:
+        db = DbManager()
+        quote = Quote()
+        quote.user_id = user_id
+        quote.content = content
+        return db.save(quote)
+    except:
+        pass
 
 def delete_quote(quote_id):
-    db = DbManager()
-    quote = db.delete(get_quote(quote_id))
-    db.close()
-    return quote
+    try:
+        db = DbManager()
+        quote = db.open().query(Quote).filter(Quote.id == quote_id).one()
+        return db.delete(quote)
+    except:
+        pass
 
 def get_user_by_id(user_id):
     db = DbManager()
     return db.open().query(User).filter(User.id == user_id).one()
 
-def get_user_by_name(username):
+def get_user_by_name(name):
     db = DbManager()
-    return db.open().query(User).filter(User.username == username).one()
+    return db.open().query(User).filter(User.name == name).one()
 
-def get_user_by_email(user_email):
+def get_user_by_email(email):
     db = DbManager()
     return db.open().query(User).filter(User.email == email).one()
 
-def create_user(email, username, password):
+def create_user(email, name, password):
     db = DbManager()
     user = User()
     user.name = name
--- server.py
+++ server.py
@@ -1,29 +1,40 @@
 import re
 from flask import Flask, session, request, redirect, render_template, flash, url_for
-from db.data_layer import get_user_by_id, get_user_by_name, get_user_by_name, create_user, get_all_quotes, get_all_quotes_for, get_quote, search_by_user_or_email, create_quote, delete_quote
+from db.data_layer import get_user_by_id, get_user_by_name, get_user_by_email, create_user, get_all_quotes, get_all_quotes_for, get_quote, search_by_user_or_email, create_quote, delete_quote
 
-'''
-USAGE:        db.<function_name>
-EXAMPLES:     db.search_by_user_or_email('Smith')
-              db.search_by_user_or_email('gmail.com')
-'''
+# '''
+# USAGE:        db.<function_name>
+# EXAMPLES:     db.search_by_user_or_email('Smith')
+#               db.search_by_user_or_email('gmail.com')
+# '''
 
 EMAIL_REGEX = re.compile(r'^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$')
 
 app = Flask(__name__)
 app.secret_key = '0d599f0ec05c3bda8c3b8a68c32a1b47'
 
+def is_blank(name, field):
+    if len(field) == 0:
+        flash('{} cannot be blank'.format(name))
+        return True
+    return False
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    quotes = get_all_quotes()
+    return render_template('index.html', all_quotes=quotes)
 
 @app.route('/create_quote', methods=['POST'])
-def create_quote():
-    pass
+def write_quote():
+    user = get_user_by_id(session['user_id'])
+    content = request.form['html_content']
+    create_quote(user.id,content)
+    return redirect(url_for('index'))
 
 @app.route('/delete/<quote_id>')
-def delete_quote(quote_id):
-    pass
+def remove_quote(quote_id):
+    delete_quote(quote_id)
+    return redirect(url_for('index'))
 
 @app.route('/search')
 def search():
@@ -31,27 +42,79 @@ def search():
     
 @app.route('/results/<query>')
 def search_users(query):
-    pass
+    users = search_by_user_or_email(query)
+    return render_template('search.html', results=users)
 
 @app.route('/user/<user_id>')
 def user_quotes(user_id):
-    pass
+    quotes=get_all_quotes_for(user_id)
+    return render_template('quote.html', all_quotes=quotes)
 
-@app.route('/authenticate')
-def authenticate():
-    pass
-
-@app.route('/login', methods=['POST'])
-def login():
-    pass
+@app.route('/authenticate/<type>')
+def authenticate(type):
+    if type == 'login':
+        return render_template('login.html')
+    elif type == 'register':
+        return render_template('register.html')
 
 @app.route('/register', methods=['POST'])
 def register():
-    pass
+    fullname = request.form['html_fullname']
+    email = request.form['html_email']
+    password = request.form['html_password']
+    confirm = request.form['html_confirm']
+
+    is_valid = True
+
+    is_valid = not is_blank('fullname', fullname)
+    is_valid = not is_blank('email', email)
+    is_valid = not is_blank('password', password)
+    is_valid = not is_blank('confirm', confirm)
+
+    if password != confirm:
+        flash('password not match')
+        is_valid = False
+    if len(password) < 6:
+        flash('pass to short')
+    if not EMAIL_REGEX.match(email):
+        flash('email is not right format')
+    
+    if is_valid:
+        try:
+            user = create_user(email, fullname, password)
+            session['user_id'] = user.id
+            session['name'] = user.name
+            return redirect(url_for('index'))
+        except:
+            flash('email already registered dude')
+    
+    return redirect(url_for('authenticate', type='register'))
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['html_email']
+    password = request.form['html_password']
+
+    try:
+        user = get_user_by_email(email)
+        
+        if password == user.password:
+            session['user_id'] = user.id
+            session['name'] = user.name
+            return redirect(url_for('index'))
+        else:
+            flash('Password not match')
+    except:
+        flash('Invalid login')
+
+    return redirect(url_for('authenticate', type='login'))
+
+
 
 @app.route('/logout')
 def logout():
-    pass
+    session.clear()
+    return redirect(url_for('index'))
 
 def setup_web_session(user):
     pass
